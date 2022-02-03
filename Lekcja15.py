d6452e01f033be2e5f37998b0ece979a8a5f6fca
bogusb <barszczbogus@gmail.com>
2022-02-03 20:29:46 +0200

    awaria internetu

--- Lekcja15.py
+++ Lekcja15.py
@@ -11,26 +11,87 @@
 # print(colors)
 # colors.append('Orange') # Add to the end of list
 
-# Append to start of list
-colors = ['Red', 'Green', 'Blue']
-print(colors)
-colors = colors[::-1]
-print(colors)
-colors.append('Orange')
-print(colors)
-colors = colors[::-1]
-print(colors)
-
-colors.insert(2, 'Yellow')
-
-colors.pop()
-colors.pop(0)
-
-del colors # to jest funkcja a nie metod
-len(colors) # to też jest funkcja
+# # Append to start of list
+# colors = ['Red', 'Green', 'Blue']
+# print(colors)
+# colors = colors[::-1]
+# print(colors)
+# colors.append('Orange')
+# print(colors)
+# colors = colors[::-1]
+# print(colors)
+#
+# colors.insert(2, 'Yellow')
+#
+# colors.pop()
+# colors.pop(0)
+#
+# del colors # to jest funkcja a nie metod
+# len(colors) # to też jest funkcja
 
 # obiekt hranit ssylku
 
-colors.remove('Green')
+# colors.remove('Green')
+#
+# real_colors = colors[:]
+#
+#
+# #Буйлук Андрей19:34
+# colors = ['Red','Green','Blue']
+# print(colors)
+# colors.append('Orange')#Add to the end of list
+# #Append to start of list
+# # colors = ['Red','Green','Blue']
+# # print(colors)
+# # colors = colors[::-1]
+# # print(colors)
+# # colors.append('Yellow')
+# # print(colors)
+# # colors = colors[::-1]
+# # print(colors)
+# # print(colors)
+# colors.insert(2,'Yellow')#Add to the position of list
+# # print(colors)
+# # colors.pop()#Delete from the end of list
+# # print(colors)
+# # colors.pop(0)#Delete from the position of list
+# # print(colors.pop(0))
+# # print(colors)
+# # del colors len(color)
+# # colors.remove('Green')#Delete by the name
+# # print(colors)
+# # print('Purle' in colors)
+# # print(len(colors))
+# new_colors = colors
+# # print(colors)
+# # print(new_colors)
+# # colors.append('Pink')
+# # print(colors)
+# # print(new_colors)
+# # new_colors.append('Cyan')
+# # print(colors)
+# # print(new_colors)
+# # real_colors = colors[:]
+# # print(colors)
+# # print(real_colors)
+# # real_colors.pop(0)
+# # colors.pop()
+# # print(colors)
+# # print(real_colors)
+# # real_colors.pop(0)
+# # real_colors.pop(0)
+# # print(colors)
+# # print(real_colors)
 
-real_colors = colors[:]
+# [19:35] Буйлук Андрей
+# Задание 1
+# В списке целых, заполненном случайными числами
+# вычислить:
+# ■ Сумму отрицательных чисел;
+# ■ Сумму четных чисел;
+# ■ Сумму нечетных чисел;
+# ■ Произведение элементов с индексами кратными 3;
+# ■ Произведение элементов между минимальным и
+# максимальным элементом;
+# ■ Сумму элементов, находящихся между первым и последним положительными элементами.
+
